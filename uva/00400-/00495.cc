633eab9aaf4b3e5cd0319a9aabcaca045640f05c
Ben Hachimori <ben.shooter2@gmail.com>
2016-03-24 20:35:22 +0900

    fixed MLE of uva 00495

--- uva/00400-/00495.cc
+++ uva/00400-/00495.cc
@@ -3,16 +3,15 @@ using namespace std;
 const int NUM = 5001, LEN = 2000;
 
 int main(){
-  static int bi[NUM][LEN]={0}, len[NUM]={0};
+  static short bi[NUM][LEN]={0}, len[NUM]={0};
   
   bi[0][0] = 0, len[0] = 0;
   bi[1][0] = 1, len[1] = 1;
   for(int i=2;i<NUM;i++){
     for(int j=0;j<len[i-1];j++)
       bi[i][j] = bi[i-1][j]+bi[i-2][j];
-    for(int j=0;j<LEN;j++){
+    for(int j=0;j+1<LEN;j++){
       if(bi[i][j]>=10){
-        if(j+1==LEN) exit(1);
         bi[i][j+1] += bi[i][j]/10;
         bi[i][j] %= 10;
       }
